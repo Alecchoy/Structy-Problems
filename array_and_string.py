cc1969aa135fabcf02b0428e7bd9f9cb0a1a2b05
Alec Choy <alecchoy@gmail.com>
2022-08-23 16:02:17 -0700

    compress

--- array_and_string.py
+++ array_and_string.py
@@ -18,4 +18,29 @@ def uncompress(s):
       
   return ''.join(result)
 #  testing 123 123 123 123
+
+
+#Write a function, compress, that takes in a string as an argument. The function should return a compressed version of the string where consecutive occurrences of the same characters are compressed into the number of occurrences followed by the character. Single character occurrences should not be changed.
+
+def compress(s):
+  s += "!"
+  result = []
+  i = 0
+  j = 0 
+
+  while j < len(s):
+    if s[i] == s[j]:
+      j += 1 
+    else:
+      num = j - i
+      if num != 1:
+        result.append(str(num))
+        result.append(s[i])
+      else:
+        result.append(s[i])
+      i = j 
       
+  return "".join(result)
+
+
+  
