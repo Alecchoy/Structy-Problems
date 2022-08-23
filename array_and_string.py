bf93a9ee3fb3168d1c62294a83f97eb8d174ba96
Alec Choy <alecchoy@gmail.com>
2022-08-23 16:08:47 -0700

    anagrams

--- array_and_string.py
+++ array_and_string.py
@@ -43,4 +43,16 @@ def compress(s):
   return "".join(result)
 
 
+  #Write a function, anagrams, that takes in two strings as arguments. The function should return a boolean indicating whether or not the strings are anagrams. Anagrams are strings that contain the same characters, but in any order.
+
+def anagrams(s1, s2):
+    return char_count(s1) == char_count(s2)
   
+def char_count(string):
+  new_hash = {}
+  for char in string:
+    if char not in new_hash:
+      new_hash[char] = 0
+    new_hash[char] += 1
+    
+  return new_hash
