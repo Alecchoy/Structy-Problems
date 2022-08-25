8148fd507db347448e57439277c470eed192972e
Alec Choy <alecchoy@gmail.com>
2022-08-25 12:31:50 -0700

    pair_sum

--- array_and_string.py
+++ array_and_string.py
@@ -76,3 +76,16 @@ def most_frequent_char(s):
   return best 
     
     
+
+
+
+def pair_sum(numbers, target_sum):
+  previous_numbers = {}
+  
+  for index, num in enumerate(numbers):
+    complement = target_sum - num
+    
+    if complement in previous_numbers:
+      return(index, previous_numbers[complement])
+    
+    previous_numbers[num] = index    
