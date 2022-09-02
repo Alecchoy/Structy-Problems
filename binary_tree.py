e83d70b7f7368d2e8405e0e1c0b665810ff802ca
Alec Choy <alecchoy@gmail.com>
2022-09-02 02:24:59 -0700

    zipper

--- binary_tree.py
+++ binary_tree.py
@@ -57,4 +57,27 @@ def get_node_value(head, index):
   return prev 
 
 
+#rite a function, zipper_lists, that takes in the head of two linked lists as arguments. The function should zipper the two lists together into single linked list by alternating nodes. If one of the linked lists is longer than the other, the resulting list should terminate with the remaining nodes. The function should return the head of the zippered linked list.
+  def zipper_lists(head_1, head_2):
+  tail = head_1
+  current1 = head_1.next
+  current2 = head_2 
+  count = 0 
   
+  while current1 is not None and current2 is not None:
+    if count % 2 == 0:
+      tail.next = current2
+      current2 = current2.next 
+    else:
+      tail.next = current1 
+      current1 = current1.next 
+    tail = tail.next
+    count += 1
+    
+  if current1 is not None:
+    tail.next = current1
+  if current2 is not None:
+    tail.next = current2 
+  
+  return head_1
+    
