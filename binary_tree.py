1d5125d3b98c272eb07cc579efdc288a76698c29
Alec Choy <alecchoy@gmail.com>
2022-09-13 22:50:06 -0700

    merge lists

--- binary_tree.py
+++ binary_tree.py
@@ -80,4 +80,44 @@ def get_node_value(head, index):
     tail.next = current2 
   
   return head_1
+
+
+
+
+merge lists
+Write a function, merge_lists, that takes in the head of two sorted linked lists as arguments. The function should merge the two lists together into single sorted linked list. The function should return the head of the merged linked list.
+
+Do this in-place, by mutating the original Nodes.
+
+You may assume that both input lists are non-empty and contain increasing sorted numbers.
+
+
+
+  class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+def merge_lists(head_1, head_2):
+  dummy = Node(None)
+  tail = dummy 
+  current_1 = head_1 
+  current_2 = head_2 
+  
+  while current_1 is not None and current_2 is not None:
+    if current_1.val < current_2.val:
+      tail.next = current_1 
+      current_1 = current_1.next 
+    else:
+      tail.next = current_2 
+      current_2 = current_2.next 
+    tail = tail.next 
+    
+  if current_1 is not None:
+    tail.next = current_1
+  if current_2 is not None:
+    tail.next = current_2 
+    
+  return dummy.next
+
     
