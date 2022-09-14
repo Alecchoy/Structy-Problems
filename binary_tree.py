2d48067b092b8d13692044fdc6e3689d222d1117
Alec Choy <alecchoy@gmail.com>
2022-09-13 22:51:08 -0700

    is_univalue_list

--- binary_tree.py
+++ binary_tree.py
@@ -120,4 +120,28 @@ def merge_lists(head_1, head_2):
     
   return dummy.next
 
+
+
+is univalue list
+Write a function, is_univalue_list, that takes in the head of a linked list as an argument. The function should return a boolean indicating whether or not the linked list contains exactly one unique value.
+
+You may assume that the input list is non-empty.
     
+
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.next = None
+
+def is_univalue_list(head):
+  current = head
+  value = current.val 
+  while current is not None:
+    if current.val != value:
+      return False 
+    current = current.next 
+  
+  return True
+
+
+  
