08a293e4ba7074e74441c7fca2fc2a73175473e0
Alec Choy <alecchoy@gmail.com>
2022-09-13 22:51:27 -0700

    longest streak

--- binary_tree.py
+++ binary_tree.py
@@ -144,4 +144,25 @@ def is_univalue_list(head):
   return True
 
 
+  longest streak
+Write a function, longest_streak, that takes in the head of a linked list as an argument. The function should return the length of the longest consecutive streak of the same value within the list.
+def longest_streak(head):
+  max_streak = 0
+  current_streak = 0 
+  prev_val = None
   
+  current_node = head
+  while current_node is not None:
+    if current_node.val == prev_val:
+      current_streak += 1 
+    else:
+      current_streak = 1 
+    
+    prev_val = current_node.val 
+    
+    if max_streak < current_streak:
+      max_streak = current_streak
+      
+    current_node = current_node.next 
+    
+  return max_streak 
