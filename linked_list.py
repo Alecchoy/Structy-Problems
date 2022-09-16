069a8fdb0d63d3a901df7d0e9b887cfd350552db
Alec Choy <alecchoy@gmail.com>
2022-09-15 23:41:08 -0700

    zipper_lists

--- linked_list.py
+++ linked_list.py
@@ -47,4 +47,28 @@ def reverse_list(head):
     current = next
   return prev
 
+def zipper_lists(head_1, head_2):
+  tail = head_1
+  current1 = head_1.next
+  current2 = head_2 
+  count = 0 
+  
+  while current1 is not None and current2 is not None:
+    if count % 2 == 0:
+      tail.next = current2
+      current2 = current2.next 
+    else:
+      tail.next = current1 
+      current1 = current1.next 
+    tail = tail.next
+    count += 1
+    
+  if current1 is not None:
+    tail.next = current1
+  if current2 is not None:
+    tail.next = current2 
+  
+  return head_1
+    
+
     
