8fb766db1678eff4e7f781d8cae051f74ec9ae28
Alec Choy <alecchoy@gmail.com>
2022-09-15 23:42:11 -0700

    merge list

--- linked_list.py
+++ linked_list.py
@@ -69,6 +69,34 @@ def zipper_lists(head_1, head_2):
     tail.next = current2 
   
   return head_1
+
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+def merge_lists(head_1, head_2):
+  dummy = Node(None)
+  tail = dummy 
+  current_1 = head_1 
+  current_2 = head_2 
+  
+  while current_1 is not None and current_2 is not None:
+    if current_1.val < current_2.val:
+      tail.next = current_1 
+      current_1 = current_1.next 
+    else:
+      tail.next = current_2 
+      current_2 = current_2.next 
+    tail = tail.next 
+    
+  if current_1 is not None:
+    tail.next = current_1
+  if current_2 is not None:
+    tail.next = current_2 
+    
+  return dummy.next
+
     
 
     
