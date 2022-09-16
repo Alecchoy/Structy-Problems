dd6c60d83cbdf085edec5ff96321d01ccd9d6dcb
Alec Choy <alecchoy@gmail.com>
2022-09-15 23:42:58 -0700

    longest_streak

--- linked_list.py
+++ linked_list.py
@@ -106,7 +106,28 @@ def is_univalue_list(head):
     current = current.next 
   
   return True
+
+def longest_streak(head):
+  max_streak = 0
+  current_streak = 0 
+  prev_val = None
   
+  current_node = head
+  while current_node is not None:
+    if current_node.val == prev_val:
+      current_streak += 1 
+    else:
+      current_streak = 1 
+    
+    prev_val = current_node.val 
+    
+    if max_streak < current_streak:
+      max_streak = current_streak
+      
+    current_node = current_node.next 
+    
+  return max_streak 
+
 
     
 
