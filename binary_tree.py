2194775682de76389c277e9057c272ed4943ff7f
Alec Choy <alecchoy@gmail.com>
2022-09-19 14:21:33 -0700

    path_finder

--- binary_tree.py
+++ binary_tree.py
@@ -112,5 +112,21 @@ def max_path_sum(root):
     return root.val
   
   return root.val + max(max_path_sum(root.left), max_path_sum(root.right))
+
+def path_finder(root, target):
+  if root is None:
+    return None
+  if root.val == target:
+    return [root.val]
+  
+  left_path = path_finder(root.left, target)
+  if left_path is not None:
+    return [root.val] + left_path 
+  right_path = path_finder(root.right, target)
+  if right_path is not None:
+    return [root.val] + right_path 
+    
+  return None
+  
     
     
