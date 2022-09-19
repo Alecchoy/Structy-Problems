5eb28a33cee07ef87c5a801b095fa4cfc04b7f09
Alec Choy <alecchoy@gmail.com>
2022-09-19 14:18:51 -0700

    bfs

--- binary_tree.py
+++ binary_tree.py
@@ -13,3 +13,22 @@ def depth_first_values(root):
     if node.left:
       stack.append(node.left)
   return values
+
+def breadth_first_values(root):
+  if not root:
+    return []
+  
+  answer = []
+  
+  queue = [root]
+  while queue:
+    node = queue.pop(0)
+    answer.append(node.val)
+    if node.left:
+      queue.append(node.left)
+    if node.right:
+      queue.append(node.right)
+  
+  
+  return answer
+    
