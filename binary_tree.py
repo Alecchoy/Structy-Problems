99378d75cd4cd67f43261c1aab62e1bef1c21360
Alec Choy <alecchoy@gmail.com>
2022-09-19 14:21:11 -0700

    max_path_sum

--- binary_tree.py
+++ binary_tree.py
@@ -102,5 +102,15 @@ def tree_min_value(root):
     if node.right:
       queue.append(node.right)
   return test 
+
+
+def max_path_sum(root):
+  if root is None:
+    return float('-inf')
+  
+  if root.left is None and root.right is None:
+    return root.val
+  
+  return root.val + max(max_path_sum(root.left), max_path_sum(root.right))
     
     
