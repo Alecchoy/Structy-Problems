4423b290738932403f512aa29cff7f56a9843998
Alec Choy <alecchoy@gmail.com>
2022-09-22 16:15:09 -0700

    tree_levels

--- binary_tree.py
+++ binary_tree.py
@@ -196,6 +196,30 @@ def all_tree_paths(root):
     paths.append([root.val, *right_path])
     
   return paths
+
+from collections import deque
+def tree_levels(root):
+  if root is None:
+    return []
+  levels = [] 
+  
+  queue = deque([(root, 0)])
+  while queue:
+    current, level_num = queue.popleft()
+    
+    if len(levels) == level_num:
+      levels.append([current.val])
+    else:
+      levels[level_num].append(current.val)
+  
+  
+    if current.left is not None:
+      queue.append((current.left, level_num + 1))
+      
+    if current.right is not None:
+      queue.append((current.right, level_num + 1))
+      
+  return levels
   
     
     
