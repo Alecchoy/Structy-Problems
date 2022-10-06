8cd716b18f584958a6cd383b32b5d90c160a6a78
Alec Choy <alecchoy@gmail.com>
2022-10-05 18:48:43 -0700

    has_path bfs iterative

--- graph.py
+++ graph.py
@@ -6,3 +6,19 @@ def has_path(graph, src, dst):
     if has_path(graph, neighbor, dst) == True:
       return True 
   return False 
+
+from collections import deque
+
+def has_path(graph, src, dst):
+  queue = deque([src])
+  
+  while queue: 
+    current = queue.popleft()
+    
+    if current == dst:
+      return True
+    
+    for neighbor in graph[current]:
+      queue.append(neighbor)
+  return False 
+
