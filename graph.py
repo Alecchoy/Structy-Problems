0a0e06ab6ef3eada9f79ce17c6ca63cb967ab582
Alec Choy <alecchoy@gmail.com>
2022-10-05 18:47:44 -0700

    has_path recursive dfs

---
+++ graph.py
@@ -0,0 +1,8 @@
+def has_path(graph, src, dst):
+  if src == dst:
+    return True
+  
+  for neighbor in graph[src]:
+    if has_path(graph, neighbor, dst) == True:
+      return True 
+  return False 
